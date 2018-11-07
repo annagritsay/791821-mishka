44e3cc7854a60663000b7f689a2a857f1a8f016d
annagritsay <anna.gritsay@bk.ru>
2018-11-08 00:23:55 +1000

    Адаптивная и векторная графика

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task("images", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/{icon-*.svg,logo-*.svg}")
     .pipe(svgstore({
       inlineSvg: true
     }))
